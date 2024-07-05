254b979711c468c5f1102dbe3295f9cd2cfa3a89
abhijay kheechee <abhijaysingh2003@gmail.com>
2024-07-05 16:52:56 +0530

    Signup component finished

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -3,7 +3,7 @@ const Login = () => {
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto ">
       <div className="w-full p-6 rounded-lg shadow-lg bg-gray-200 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
         <h1 className="text-3xl font-semibold text-center text-gray-300">
-          Login
+          Login to
           <span className="text-indigo-400 "> Chat App</span>
         </h1>
         <form>
