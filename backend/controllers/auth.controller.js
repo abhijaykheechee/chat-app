1622b8134535563315cb63817e151f9278fae50f
abhijay kheechee <abhijaysingh2003@gmail.com>
2024-07-06 23:49:23 +0530

    Signup hook completed and jwt typeerror fixed

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,5 @@
-import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import User from "../models/user.model.js";
 import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signup = async (req, res) => {
@@ -7,19 +7,20 @@ export const signup = async (req, res) => {
     const { fullName, username, password, confirmPassword, gender } = req.body;
 
     if (password !== confirmPassword) {
-      return res.status(400).json({ error: "Passwords do not match" });
+      return res.status(400).json({ error: "Passwords don't match" });
     }
+
     const user = await User.findOne({ username });
 
     if (user) {
       return res.status(400).json({ error: "Username already exists" });
     }
 
-    //Password Hashing
+    // HASH PASSWORD HERE
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    //avatar
+    // https://avatar-placeholder.iran.liara.run/
 
     const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
     const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
@@ -33,22 +34,22 @@ export const signup = async (req, res) => {
     });
 
     if (newUser) {
-      //Generate JWT token
+      // Generate JWT token here
       generateTokenAndSetCookie(newUser._id, res);
       await newUser.save();
 
       res.status(201).json({
-        _id: newUser.id,
+        _id: newUser._id,
         fullName: newUser.fullName,
         username: newUser.username,
         profilePic: newUser.profilePic,
       });
     } else {
-      res.status(400).json({ error: "Invalid User Data" });
+      res.status(400).json({ error: "Invalid user data" });
     }
   } catch (error) {
     console.log("Error in signup controller", error.message);
-    res.status(500).json({ error: "Server Error" });
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
@@ -62,7 +63,7 @@ export const login = async (req, res) => {
     );
 
     if (!user || !isPasswordCorrect) {
-      res.status(400).json({ error: "Invalid username or password" });
+      return res.status(400).json({ error: "Invalid username or password" });
     }
 
     generateTokenAndSetCookie(user._id, res);
